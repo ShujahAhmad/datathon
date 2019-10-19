187db8d04c2a90f4067942a5bdfe9cc840f8070e
OrigamiAztec <origamiaztec@gmail.com>
2019-10-19 16:56:52 -0500

    Adding option for sensor number and example of graphing with Javascript

--- visualization_scripts.js
+++ visualization_scripts.js
@@ -1,1 +1,11 @@
-//test
+var x = [];
+for (var i = 0; i < 500; i ++) {
+	x[i] = Math.random();
+}
+
+var trace = {
+    x: x,
+    type: 'histogram',
+  };
+var data = [trace];
+Plotly.newPlot('myDiv', data, {}, {showSendToCloud: true});
