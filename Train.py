b050671bcc4a5ac23ab1f2f5183e818083b6bc58
ScaryMonkey83 <Jalehndet11@gmail.com>
2019-10-19 13:54:31 -0500

    This is a test

--- Train.py
+++ Train.py
@@ -2,13 +2,15 @@ import pandas as pd
 import seaborn as s
 
 if __name__ == '__main__':
-    train = pd.read_csv("data/equip_failures_training_set.csv")
-    train.replace('na', 0)
-    print(train)
+    train = pd.read_csv("data/equip_failures_training_set.csv", na_values='na')
 
-    s.pairplot(train,
+    train.fillna(0, inplace=True)
+
+
+    g = s.pairplot(train,
                x_vars=["sensor1_measure", "sensor3_measure"],
                y_vars=["sensor1_measure", "sensor3_measure"],
                hue='target')
-
-
+    # s.pairplot(train,
+    #            vars=["sensor1_measure", "sensor3_measure"],
+    #            hue='target')
